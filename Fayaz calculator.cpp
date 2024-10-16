b73cf4a23aa138b0f84857617c404e479196e3ba
fayaz wazir <fayazwazir636363@gmail.com>
2024-10-16 21:22:12 +0500

    Add files via upload

---
+++ Fayaz calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,addittion,subtraction,product,division;
+	cout<<"Enter value for a"<<endl;
+	cin>>a;
+		cout<<"Enter value for b"<<endl;
+		cin>>b;
+		addittion=a+b;
+		subtraction=a-b;
+		product=a*b;
+		division=a/b;
+		cout<<"The addittion of a and b ="<<addittion<<endl;
+		cout<<"The subtraction of a and b ="<<subtraction<<endl;
+		cout<<"The product of a and b ="<<product<<endl;
+		cout<<"The division of a and b ="<<division<<endl;
+		return 0;
+		
+		
+		
+		
+		
+}
